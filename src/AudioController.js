f4b0c57f995db3b434e81d7f61c56c0927b1e23b
Xueting D <dxt_jenny@outlook.com>
2023-11-11 22:48:07 -0800

    add bgm controller

--- src/AudioController.js
+++ src/AudioController.js
@@ -5,32 +5,48 @@ import unmuteIcon from './assets/unmute-icon.png';
 const AudioController = ({ audioFile }) => {
 
     const [isMuted, setIsMuted] = useState(true); // Start muted to comply with autoplay policies
-    const audioRef = useRef(new Audio(audioFile));
+    const audioRef = useRef(null);
     
-    // Play music once the user interacts
-    const playMusic = () => {
-        // Check if the audio is already playing
-        if (audioRef.current.paused) {
-            // Attempt to play and catch any errors
-            audioRef.current.play().then(() => {
-            console.log('Audio started playing');
-            }).catch((error) => {
-            console.error('Error occurred while trying to play audio:', error);
-            // Handle the error here. For example, you might want to display an alert to the user.
+    useEffect(() => {
+        // Create new audio object with the file
+        const audio = new Audio(audioFile);
+        audio.loop = true;
+        audio.volume = 0.5;
+        audio.muted = isMuted;
+        audio.autoplay = true;
+        audioRef.current = audio;
+    
+        // Attempt to play the audio
+        audio.play().catch((error) => {
+          console.error('Autoplay was prevented:', error);
+        });
+        
+        // Cleanup function to pause the audio when component unmounts
+        return () => {
+          audio.pause();
+        };
+      }, [audioFile, isMuted]);
+    
+      const toggleMute = () => {
+        if (audioRef.current) {
+          audioRef.current.muted = !isMuted;
+          setIsMuted(!isMuted);
+    
+          // If unmuting, make sure the audio plays
+          if (isMuted) {
+            audioRef.current.play().catch((error) => {
+              console.error('Error occurred while trying to play audio:', error);
             });
-        } else {
-            // If audio is playing, pause it
-            audioRef.current.pause();
+          }
         }
-        setIsMuted(!isMuted);
-    };
-
-
-    return (
-        <button onClick={playMusic} className="mute-button">
-        <img src={isMuted ? unmuteIcon : muteIcon} alt="Mute/Unmute" />
+      };
+    
+      return (
+        <button onClick={toggleMute} className="mute-button">
+          <img src={isMuted ? unmuteIcon : muteIcon} alt="Mute/Unmute" />
         </button>
-    );
-};
-
-export default AudioController;
+      );
+    };
+    
+    export default AudioController;
+    
