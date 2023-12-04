09a8ff4175d07dcf0f2f92c62f47aa85177b471a
dingjunyu888 <dingjunyu888@gmail.com>
2023-12-03 23:55:43 -0800

    pom.xml added

--- back_end/wheel-of-fortune-database/src/main/java/com/example/demo/ScoreController.java
+++ back_end/wheel-of-fortune-database/src/main/java/com/example/demo/ScoreController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
+//Delete import
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
